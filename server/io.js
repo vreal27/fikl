2a40017fb3a581661893dfeff4c613c1c2989d93
Eddie Villareal <eddie.villareal27@gmail.com>
2019-02-04 16:24:48 -0800

    logic for users added

--- server/io.js
+++ server/io.js
@@ -3,6 +3,8 @@ import socketio from 'socket.io'
 export default function(server) {
   const io = socketio(server)
 
+  const users = []
+
   io.on('connection', function(socket){
 
     // Join code/room
@@ -11,6 +13,24 @@ export default function(server) {
       })
 
     // Add/delete item
+
+      socket.on('add item', user => {
+        users.forEach((u,i) => {
+          if(u.username === user) {
+            users[i].isAdded = true
+            users[i].step = "wait"
+          }
+        })
+      })
+
+      socket.on('remove item', user => {
+        users.forEach((u, i) => {
+          if(u.username === user) {
+            users[i].isRemoved = true
+            users[i].step = "wait"
+          }
+        })
+      })
 
     // Category
       socket.on('set category', category => {
@@ -24,8 +44,16 @@ export default function(server) {
       socket.on('set username', username => {
         socket.emit('set username', username)
       })
-
-
+    
+    // Add user
+    socket.on('new user', user => {
+      users.push({
+        username: user,
+        step: 'wait',
+        isAdded: false,
+        isRemoved: false
+      })
+    })
     
 
     console.log('User has connected to socket server')
